bd83d9a9419c22b1f4c86371e6b11b5ed09503c7
jonnyrobbie <marcodv@seznam.cz>
2015-09-29 23:29:45 +0200

    initial commit

---
+++ seznam.user.js
@@ -0,0 +1,31 @@
+// ==UserScript==
+// @name           Seznam iFrame
+// @description    Nahradi poznamky na Seznamu iframem s odkazem na vlastni html
+// @namespace      https://greasyfork.org/en/users/98-jonnyrobbie
+// @author         JonnyRobbie
+// @include        /^https?:\/\/(www\.)?seznam\.cz\/.*$/
+// @version        1.0
+// ==/UserScript==
+
+var url = ["", "", ""];
+var size = [340, 312, 0];
+window.scrIframes = {};
+function main() {
+	for (i=0;i<url.length;i++) {
+		if (url[i] != "") {
+			console.log("Seznam " + i);
+			var note = document.getElementsByClassName("notes-list")[0].childNodes[i].childNodes[1];
+			console.log("var " + i);
+			scrIframes[i] = new Object
+			scrIframes[i] = document.createElement("iframe");
+			scrIframes[i].src = url[i];
+			scrIframes[i].style.border = "none";
+			scrIframes[i].style.height = size[i] + "px";
+			scrIframes[i].style.width = "100%";
+			note.innerHTML = "";
+			note.appendChild(scrIframes[i]);
+		}
+	}
+}
+
+main();
